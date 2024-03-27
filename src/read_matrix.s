08921f15184d726d3f986817d92eab57de366448
Nick Liu <entomophile@NickdeMacBook-Air.local>
2024-03-27 22:54:28 +0800

    can read matrix

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -26,16 +26,66 @@
 read_matrix:
 
     # Prologue
-	
+    addi sp, sp, -24
+    sw ra, 20(sp)
+    sw a0, 8(sp)
+    sw a1, 4(sp)
+    sw a2, 0(sp)
+    sw s0, 12(sp)
+    sw s1, 16(sp)
 
+    #prepare arg for fopen
+    add a1, a0, x0
+    add a2, x0, x0
+    jal fopen 
+    add s0, a0, x0 #s0: file descriptor
+    #arg: a1: ptr to string with filename; 
+    #a2: int to permission. 0: read only
+    #ret: a0: file descrip
 
+    #get row, col size of a mat, store size of matrix in bytes into s1
+    addi a0, x0, 8
+    jal malloc
 
+    #read
+    add a1, s0, x0
+    add a2, a0, x0
+    addi a3, x0, 8
+    jal fread
 
+    #get val
+    lw t0, 0(a2) #row num
+    lw t2, 4(sp)
+    sw t0, 0(t2) #store row number in the reference at a1
+    lw t1, 4(a2) #col num
+    lw t2, 0(sp)
+    sw t1, 0(t2) #store col number in reference at a2
 
+    #calc array size in bytes
+    addi s1, x0, 4
+    mul s1, s1, t0
+    mul s1, s1, t1 #size of array
+    add a0, x0, s1
+    jal malloc
+    
+    #malloc arg: a0, number of bytes; 
+    #ret: a0, ptr
 
+    #set arguments for fread of whole array
+    add a2, a0, x0 #strict link with above malloc
+    add a1, s0, x0
+    add a3, s1, x0
+    add s1, a0, x0
+    jal fread
+    #check fread error
 
+    #load return
+    add a0, s1, x0
 
     # Epilogue
-
+    lw ra, 20(sp)
+    lw s0, 12(sp)
+    lw s1, 16(sp)
+    addi sp, sp, 24
 
     ret
--- tests/assembly/test_read_matrix.s
+++ tests/assembly/test_read_matrix.s
@@ -2,16 +2,26 @@
 .import ../../src/utils.s
 
 .data
-file_path: .asciiz "inputs/test_read_matrix/test_input.bin"
+file_path: .asciiz "/Users/entomophile/CS/CS61Cprojects/proj2/CS61Cprojects/src/test_input.bin"
+vector0: .word 0 0
 
 .text
 main:
+    #load addresses into reg
+    la s0 file_path
+    la s1 vector0
     # Read matrix into memory
-
-
+    add a0, s0, x0
+    add a1, s1, x0
+    addi a2, s1, 4
+    jal read_matrix
 
     # Print out elements of matrix
+    lw a1, 0(s1)
+    lw a2, 4(s1)
+    jal print_int_array
 
 
 
     # Terminate the program
+    jal exit
